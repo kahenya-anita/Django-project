d500245274c9c9834b5a8699408cda72ccb3cbdd
kahenya-anita <anitakahenya1@gmail.com>
2020-10-06 15:43:34 +0300

    Query data to db

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Anita',
-        'title': 'Blog post 1',
-        'content': 'First post',
-        'date-posted': 'Aug 21, 2020'
-    },
-    {
-        'author': 'Mary',
-        'title': 'Blog post 2',
-        'content': 'Second post',
-        'date-posted': 'Aug 21, 2020'
-    }
-]
+
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
